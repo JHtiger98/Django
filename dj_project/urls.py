476c2d6c04b782537a22aa2ecba8ac0cd39867b8
Jhtiger98 <smarthun0812@gmail.com>
2023-02-14 00:51:31 +0900

    '0213'

--- dj_project/urls.py
+++ dj_project/urls.py
@@ -19,5 +19,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', include('homepage.urls'))
+    path('', include('homepage.urls')),
+    path('markdownx/', include('markdownx.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
